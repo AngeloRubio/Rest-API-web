086caeeec07d796a5fb55551f6963e126a0bec13
Angelo <angelo.rubiomer@ug.edu.ec>
2023-07-28 22:55:22 -0500

    Agregar archivos de proyecto.

---
+++ Models/Student.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleAppAPI.Models;
+
+public partial class Student
+{
+    public int StudentId { get; set; }
+
+    public string? Name { get; set; }
+
+    public string? Lastname { get; set; }
+}
